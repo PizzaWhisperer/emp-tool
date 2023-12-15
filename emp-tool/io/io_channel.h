bb7ba963c880f894efe13eca4efddc82854c354f
Kasra EdalatNejad <kasra.edalat@gmail.com>
2023-12-15 18:52:19 +0100

    Add get_sent_counter

--- emp-tool/io/io_channel.h
+++ emp-tool/io/io_channel.h
@@ -8,16 +8,21 @@
 
 namespace emp {
 template<typename T>
-class IOChannel { public:
+class IOChannel { 
+	public:
 	uint64_t counter = 0;
+	
 	void send_data(const void * data, size_t nbyte) {
 		counter +=nbyte;
-		//std::cout << "counter " << counter << std::endl;
 		derived().send_data_internal(data, nbyte);
 	}
 
 	void print_counter() {
 		std::cout << "counter" << counter << std::endl;
+	}
+	
+	uint64_t get_sent_byte_count(){
+		return counter;
 	}
 
 	void recv_data(void * data, size_t nbyte) {
